23d9a20c648f99b6d46d26a8995acd23dd37a1e4
YaBoiJD <jellisduncan@gmail.com>
2020-04-16 01:47:53 -0500

    add alias to ping and destription to ping command

--- src/plugins/commands/ping.js
+++ src/plugins/commands/ping.js
@@ -3,5 +3,7 @@
 module.exports = {
     run: async(client, message, args) => {
         message.reply(`Pong!\n ${client.ws.ping}ms is the latency`);
-    }
+    },
+    aliases: ['p'],
+    description: 'Tells the user the latency.'
 }
