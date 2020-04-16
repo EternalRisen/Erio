b745a33ee64573f660e929fcd1a534df61ac6899
YaBoiJD <jellisduncan@gmail.com>
2020-04-16 02:16:34 -0500

    remove args from params

--- src/plugins/commands/ping.js
+++ src/plugins/commands/ping.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-    run: async(client, message, args) => {
-        message.reply(`Pong!\n ${client.ws.ping}ms is the latency`);
-    },
-    aliases: ['p'],
-    description: 'Tells the user the latency.'
-}
+	run: async (client, message) => {
+		message.reply(`Pong!\n ${client.ws.ping}ms is the latency`);
+	},
+	aliases: ['p'],
+	description: 'Tells the user the latency.',
+};
