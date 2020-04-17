e9684080bc5ecd8e0fec7aa5b9fdaa51df4c1614
YaBoiJD <jellisduncan@gmail.com>
2020-04-17 00:26:29 -0500

    add returning line after last line in code and add say command

---
+++ src/plugins/commands/say.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+	/* eslint-disable */
+    run: (client, message, args) => {
+        /* eslint-enable */
+		let sayMessage = args.join(' ');
+		if (sayMessage === '') {
+			sayMessage = args.join(' ') + 'Nothin to say eh?' + message.author;
+		}
+		message.delete();
+		message.channel.send(sayMessage);
+	},
+	aliases: ['s'],
+	description: 'deletes the command message and returns with a sent message containing the content after the command name',
+};
