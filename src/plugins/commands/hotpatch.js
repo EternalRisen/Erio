aa88f188893bf7cf653b67966d9b6037290dc9fe
JD <jellisduncan@gmail.com>
2020-05-30 02:57:28 -0500

    add a hotpatch command

---
+++ src/plugins/commands/hotpatch.js
@@ -0,0 +1,48 @@
+'use strict';
+const { isOperator } = require('../../structs/verification.js');
+const fs = require('fs').promises;
+const path = require('path');
+const { checkCommandModule, checkProperties } = require('../../structs/validate.js');
+
+module.exports = {
+	/* eslint-disable */
+    run: async (client, message, args) => {
+        /* eslint-enable */
+		if (isOperator(message.author.id)) {
+			const hotpatchChoice = args[0];
+			if (hotpatchChoice.toLowerCase() === 'commands') {
+				console.log('Hotpatching Commands...');
+				client.commands.delete();
+				client.commands = new Map();
+				const files = await fs.readdir(path.join(__dirname, '../../plugins/commands'));
+				for (const file of files) {
+					if (file.endsWith('.js')) {
+						try {
+							const cmdName = file.substring(0, file.indexOf('.js'));
+							const cmdModule = require(path.join(__dirname, '../../plugins/commands', file));
+							if (checkCommandModule(cmdName, cmdModule)) {
+								if (checkProperties(cmdName, cmdModule)) {
+									const { aliases } = cmdModule;
+									client.commands.set(cmdName, cmdModule);
+									if(aliases.length !== 0) {
+										aliases.forEach(alias => client.commands.set(alias, cmdModule));
+									}
+									console.log(`Command loaded:  ${cmdName}:  ${cmdModule.description}`);
+								}
+							}
+						}
+						catch (err) {
+							console.log(err);
+						}
+					}
+				}
+				message.reply('Commands have been hotpatched. :thumbsup:');
+			}
+		}
+		else {
+			return;
+		}
+	},
+	aliases: [],
+	description: 'Hotpatches Events(TODO) and Commands.',
+};
