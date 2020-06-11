7767a1a23824d1d69a3631b23d8f0cfa7e52fb11
JD <jellisduncan@gmail.com>
2020-06-11 18:59:47 -0500

    add rate command

---
+++ src/plugins/commands/rate.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+	/* eslint-disable */
+	run: async (client, message, args) => {
+        /* eslint-enable */
+        const rateString = args.join(' ');
+        let rating = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+		message.reply(`I rate ${rateString} a ${rating[Math.floor(Math.random() * rating.length)]}/10`);
+	},
+	aliases: ['pick'],
+	description: 'Makes a random choice between the choices given.',
+};
