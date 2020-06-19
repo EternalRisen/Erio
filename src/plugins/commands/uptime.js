f096698c45804746dfef8dbe692ab30c6a8d4628
JD <jellisduncan@gmail.com>
2020-06-19 10:37:17 -0500

    add uptime command

---
+++ src/plugins/commands/uptime.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+	/* eslint-disable */
+    run: async (client, message, args) => {
+        /* eslint enable */
+        let totalSeconds = Math.floor(client.uptime / 1000); 
+        let days = Math.floor(totalSeconds / 86400); 
+        let hours = Math.floor(totalSeconds / 3600); 
+        totalSeconds %= 3600; 
+        let minutes = Math.floor(totalSeconds / 60); 
+        let seconds = Math.floor(totalSeconds % 60);
+        let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+        message.channel.send(uptime);
+    },
+    aliases: [],
+    description: 'Returns the amount of time the bot has been online.',
+    usage: 'uptime',
+}
