c71fdd72109c24c1e8ca09c9b03d26e055720c08
JD <jellisduncan@gmail.com>
2020-06-25 23:12:43 -0500

    eslint stuff...

--- src/plugins/commands/avatar.js
+++ src/plugins/commands/avatar.js
@@ -3,23 +3,24 @@
 const { MessageAttachment } = require('discord.js');
 
 function getAvatar(user) {
-    return `${user.displayAvatarURL().replace('.webp', '.png')}?size=2048`;
+	return `${user.displayAvatarURL().replace('.webp', '.png')}?size=2048`;
 }
 
 module.exports = {
-    /* eslint-disable */
+	/* eslint-disable */
     run: async (client, message, args) => {
         /* eslint-enable */
-        client.users.fetch(args[0]).then(user => {
-            const avatar = new MessageAttachment(getAvatar(user));
-            message.channel.send(`${user.tag}'s avatar:`, avatar);
-        }).catch(e => {
-            let user = message.mentions.users.first() || message.author;
-            const avatar = new MessageAttachment(getAvatar(user));
-            message.channel.send(`${user.tag}'s avatar:`, avatar);
-        });
-    },
-    aliases: ['a'],
-    description: 'returns with the user\'s avatar',
-    usage: 'avatar <user>'
-}
+		client.users.fetch(args[0]).then(user => {
+			const avatar = new MessageAttachment(getAvatar(user));
+			message.channel.send(`${user.tag}'s avatar:`, avatar);
+		}).catch(e => {
+			console.log(`We just need to log this somewhere else in the future when I add other things. ${e}`);
+			const user = message.mentions.users.first() || message.author;
+			const avatar = new MessageAttachment(getAvatar(user));
+			message.channel.send(`${user.tag}'s avatar:`, avatar);
+		});
+	},
+	aliases: ['a'],
+	description: 'returns with the user\'s avatar',
+	usage: 'avatar <user>',
+};
