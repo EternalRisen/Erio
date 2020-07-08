f060340533b4a383ee5828cc87d54d4439dd3b71
JD <jellisduncan@gmail.com>
2020-07-08 04:51:53 -0500

    add update command

---
+++ src/plugins/commands/update.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import Discord from 'discord.js';
+import { stringify } from 'querystring';
+
+module.exports = {
+    run: async (client: { devs: string | string[]; }, message: Discord.Message, args: string[]) => {
+        if (!client.devs.includes(message.author.id)) return;
+        await require('child_process').exec(`git pull --rebase ${process.env.GITREPO || 'https://github.com/yaboijd/erio'}`, {stdio: 'inherit'}, (stderr: string, stdout: string) => {
+            message.channel.send(`Update status:\n${stdout}\n${stderr}`)
+        });
+    },
+    aliases: [],
+    description: 'runs a git pull to update the local files',
+    type: 'dev',
+    usage: 'update',
+}
