9619aafd8e70223f82ad668ddd73c2c2fd01a59b
jd <jellisduncan@gmail.com>
2020-08-21 21:40:41 -0500

    more fucking hell thanks to discord 🖕🖕🖕

--- src/plugins/commands/kick.ts
+++ src/plugins/commands/kick.ts
@@ -1,11 +1,16 @@
 'use string';
 
-import Discord from 'discord.js';
+import Discord, { GuildMember, User } from 'discord.js';
 
 module.exports = {
     run: async (client: Discord.Client, message: Discord.Message, args: string[]) => {
         if (!message.member?.permissions.has(['KICK_MEMBERS', 'MANAGE_GUILD', 'ADMINISTRATOR'])) return;
         let userID = args[0];
+        let rsn = args[1];
+        let err;
+        if (rsn === '') {
+            rsn = 'No reason specified.'
+        }
         try {
             userID = userID.replace('<', '');
             userID = userID.replace('@', '');
@@ -14,21 +19,38 @@ module.exports = {
         } catch (e) {
             console.log('owo');
         }
-        let member;
+        let member: Discord.GuildMember | undefined;
+        let rejectionReason;
+
+        function permCheck() {
+            try {
+                if (message.member?.id === message.guild?.ownerID) {
+                    return false;
+                } else if ((member as GuildMember).roles.highest.rawPosition >= message.member!.roles.highest.rawPosition) {
+                    rejectionReason = 'The user is either a higher up or is the same authority level as you.';
+                    return true;
+                } else if (member?.id === message.guild?.ownerID) {
+                    rejectionReason = 'You can\'t kick the owner';
+                    return true;
+                }
+            } catch (e) {
+                err = e;
+            }
+        }
+
         try {
             member = await message.guild?.members.cache.find(u => u.id === userID);
         } catch (e) {
             return message.channel.send(`member ${member} was not found.`)
         }
-        if (member?.kickable) {
-            try {
-                member.kick();
-                message.channel.send(`Member ${(member as unknown as Discord.User).tag}(${(member as unknown as Discord.User).id}) has been kicked`);
-            } catch (e) {
-                message.channel.send(`There was a problem with your request:  ${e.message}`);
-            }
-        } else {
-            return message.reply('I\'m Probably lacking the permissions to kick this user or they are too high up in the hierarchy');
+        
+        try {
+            if (permCheck()) return message.reply(rejectionReason);
+            member?.kick();
+            message.channel.send(`Member ${member} has been kicked`);
+            (member as unknown as User).send(`You have been banned from ${message.guild} for the following reason:  ${rsn}`)
+        } catch (e) {
+            message.channel.send(`There was a problem with your request:  ${e.message}`);
         }
     },
     aliases: ['k'],
---
+++ src/plugins/commands/unban.ts
@@ -0,0 +1,42 @@
+'use strict';
+
+import Discord from 'discord.js'
+
+module.exports = {
+    run: async (client: Discord.Client, message: Discord.Message, args: string[]) => {
+        if (!message.member?.permissions.has(['BAN_MEMBERS', 'MANAGE_GUILD', 'ADMINISTRATOR'])) return;
+        let userID = args[0];
+        let rsn = args[1];
+
+        if (rsn === '') {
+            rsn = 'No reason specified.'
+        }
+
+        // Some retarded ass check that I have to fucking do bc fuck you discord 🖕🖕🖕
+        async function checkBans() {
+            try {
+                let member = await message.guild?.members.cache.find(u => u.id === userID);
+                if (userID === message.guild?.ownerID) { return false; }
+                if (member!.id === userID) { return false; } else {return true;}
+            } catch (e) {
+                return true;
+            }
+        }
+
+        const banned = checkBans();
+
+        try {
+            let guildMember = await client.users.fetch(userID);
+            if (await banned === false) return message.reply('this user is not banned.');
+            message.guild?.members.unban(guildMember);
+            message.channel.send(`member ${guildMember} has been unbanned`);
+            guildMember.send(`you have been unbanned from ${message.guild} for the following reason:  ${rsn}`)
+        } catch (e) {
+            return message.channel.send(`There was a problem processing this request.  ${e.message}`);
+        }
+    },
+    aliases: [],
+    description: 'Unbans a member',
+    type: 'mod',
+    usage: 'unban <member>'
+}
