f2e0685cc26f10069bd6182201e1751b82050337
jd <jellisduncan@gmail.com>
2020-08-30 04:43:19 -0500

    try something for the uptime

--- src/plugins/commands/uptime.ts
+++ src/plugins/commands/uptime.ts
@@ -11,6 +11,7 @@ module.exports = {
         let totalSeconds = Math.floor(client.uptime / 1000); 
         let days = Math.floor(totalSeconds / 86400); 
         let hours = Math.floor(totalSeconds / 3600); 
+        hours = (hours - (days * 24));
         totalSeconds %= 3600; 
         let minutes = Math.floor(totalSeconds / 60); 
         let seconds = Math.floor(totalSeconds % 60);
