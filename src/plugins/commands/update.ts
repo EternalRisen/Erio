0a8070270a7139d8a662fef32a2d94a3d438ec41
jd <jellisduncan@gmail.com>
2020-08-31 22:44:43 -0500

    disable rebasing >_>

--- src/plugins/commands/update.ts
+++ src/plugins/commands/update.ts
@@ -10,7 +10,7 @@ module.exports = {
     run: async (client: { devs: string | string[]; }, message: Discord.Message, args: string[]) => {
         // TODO:  Add suport for forcing a pull if fails (usually with heroku failing a git pull)
         if (!client.devs.includes(message.author.id)) return;
-        await require('child_process').exec(`git pull --rebase ${process.env.GITREPO || 'https://github.com/yaboijd/erio'}`, {stdio: 'inherit'}, (stderr: string, stdout: string) => {
+        await require('child_process').exec(`git pull ${process.env.GITREPO || 'https://github.com/yaboijd/erio'}`, {stdio: 'inherit'}, (stderr: string, stdout: string) => {
             message.channel.send(`Update status:\n${stdout}\n${stderr}`)
         });
     },
