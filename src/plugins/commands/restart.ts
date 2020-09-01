f399c5d091552baac765a5765b9ed6291402beca
jd <jellisduncan@gmail.com>
2020-09-01 12:46:53 -0500

    change how the bot dumps restart logs

--- src/plugins/commands/restart.ts
+++ src/plugins/commands/restart.ts
@@ -9,15 +9,10 @@ import Discord = require('discord.js');
 const { exec } = require('child_process');
 
 module.exports = {
-	run: async (client: { devs: string | string[]; users: { fetch: (arg0: any) => Promise<Discord.User>; }; user: { username: any; }; }, message: Discord.Message, args: Array<string>) => {
-		// TODO:  Add Heroku restart support
+	run: async (client: any, message: Discord.Message, args: Array<string>) => {
 		if (client.devs.includes(message.author.id)) {
-			// TODO: Change this to send to the dump area
-			for (const dev of client.devs) {
-				client.users.fetch(dev).then((user: Discord.User) => {
-					user.send(`${client.user.username} is being restarted by ${message.author.tag}(${message.author.id})`);
-				});
-			}
+			let logChannel = await client.channels.cache.get((process.env.BOTLOG as string));
+			(logChannel as Discord.TextChannel).send(`${client.user.username} is being restarted by ${message.author.tag}(${message.author.id})`)
 
 			exec('npm start');
 
