34ddfb3a4f36ff4f1f616bc6ca2ac6cbecb7a179
jd <jellisduncan@gmail.com>
2020-09-05 15:05:03 -0500

    hmm

--- src/plugins/commands/update.ts
+++ src/plugins/commands/update.ts
@@ -8,9 +8,8 @@ import Discord from 'discord.js';
 
 module.exports = {
     run: async (client: { devs: string | string[]; }, message: Discord.Message, args: string[]) => {
-        // TODO:  Add suport for forcing a pull if fails (usually with heroku failing a git pull)
         if (!client.devs.includes(message.author.id)) return;
-        await require('child_process').exec(`git pull ${process.env.GITREPO || 'https://github.com/yaboijd/erio'}`, {stdio: 'inherit'}, (stderr: string, stdout: string) => {
+        await require('child_process').exec(`git --rebase pull ${process.env.GITREPO || 'https://github.com/yaboijd/erio'}`, {stdio: 'inherit'}, (stderr: string, stdout: string) => {
             message.channel.send(`Update status:\n${stdout}\n${stderr}`)
         });
     },
